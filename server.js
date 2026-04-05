67eb713e6ff077863c67acee546dc5fac91db777
Habibulla403 <mdporanmolla776@gmail.com>
2026-04-05 22:18:12 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.js";
 import songRoutes from "./routes/songs.js";
+import uploadRoutes from "./routes/upload.js";
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
 app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -18,6 +27,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/", (req, res) => res.json({ message: "WaveTrack API running" }));
 
